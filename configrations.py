fad9780347a12ad2bfec09e2f42a9ad94da82260
vishalshinde-30 <vishal.shinde4795@gmail.com>
2026-01-06 21:53:45 +0530

    Added .gitignore

--- configrations.py
+++ configrations.py
@@ -1,11 +1,15 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://vishalshinde4795:12345@cluster0.zu4i7b9.mongodb.net/?appName=Cluster0"
+load_dotenv()
 
-# Create a new client and connect to the server
+uri = os.getenv("MONGODB_URI", "mongodb+srv://vishalshinde4795:12345@cluster0.zu4i7b9.mongodb.net/?appName=Cluster0")
+database_name = os.getenv("DATABASE_NAME", "todo_db")
+collection_name = os.getenv("COLLECTION_NAME", "todo_data")
+
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-db  =  client.todo_db
-collection  =  db["todo_data"]
+db = client[database_name]
+collection = db[collection_name]
