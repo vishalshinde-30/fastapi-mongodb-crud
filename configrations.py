1b8fbc074e64b517a13d0ba150b0a8e23e024bc0
vishalshinde-30 <vishal.shinde4795@gmail.com>
2026-01-06 22:06:11 +0530

    initial commit

--- configrations.py
+++ configrations.py
@@ -3,9 +3,24 @@ from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-uri = os.getenv("MONGODB_URI", "mongodb+srv://vishalshinde4795:12345@cluster0.zu4i7b9.mongodb.net/?appName=Cluster0")
+load_dotenv('.env.local')  
+load_dotenv()  
+
+uri = os.getenv("MONGODB_URI")
+
+if not uri:
+    raise ValueError("""
+    MONGODB_URI environment variable is not set.
+    
+    Please create a .env file with:
+    MONGODB_URI=your_mongodb_connection_string
+    DATABASE_NAME=todo_db
+    COLLECTION_NAME=todo_data
+    
+    Or if developing locally, create .env.local with your credentials.
+    """)
+
 database_name = os.getenv("DATABASE_NAME", "todo_db")
 collection_name = os.getenv("COLLECTION_NAME", "todo_data")
 
